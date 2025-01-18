d70facc899e685157720ad13e0e8416383167e7a
Ben <benschumacher0@gmail.com>
2025-01-18 20:38:32 +0100

    add pipeline@

---
+++ aiko2/core/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .message import Message
+from .conversation import Conversation
--- aiko2/pipeline/base_pipeline.py
+++ aiko2/pipeline/base_pipeline.py
@@ -1,21 +1,52 @@
 from abc import ABC, abstractmethod
+from aiko2.core import Conversation, Message, User
 
 class BasePipeline(ABC):
     """
     Base class for all pipelines.
     This class controls the retrieval and generation process.
-    At minimum, the pipeline takes an input query and returns a response.
+    At minimum, the pipeline should be able to generate a response
+    using the generate method.
     """
 
     @abstractmethod
-    def query(self, query: str) -> str:
+    def generate(self, conversation: Conversation) -> Message | None:
         """
-        Query the pipeline with a given input and return a response.
+        Generate a response based on the conversation.
+
+        Parameters
+        ----------
+        conversation : Conversation
+            The conversation to generate a response from.
+
+        Returns
+        -------
+        Message | None
+            The generated response.
+            If no response was generated, return None.
+            This can happen if the pipeline is setup to decide 
+            whether to generate a response or not, for example if
+            the message wasn't directed at the AI.
         """
         pass
 
-    def __call__(self, query: str) -> str:
+    def __call__(self, conversation: Conversation) -> Message | None:
         """
-        Allow the pipeline to be called as a function.
+        Call the pipeline to generate a response.
+
+        Parameters
+        ----------
+        conversation : Conversation
+            The conversation to generate a response from.
+
+        Returns
+        -------
+        Message | None
+            The generated response.
+            If no response was generated, return None.
+            This can happen if the pipeline is setup to decide 
+            whether to generate a response or not, for example if
+            the message wasn't directed at the AI.
         """
-        return self.query(query)
+
+        return self.generate(conversation)
