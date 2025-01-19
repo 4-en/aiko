951ad737067deb44471a837f701a295393b29ddd
Ben <benschumacher0@gmail.com>
2025-01-19 17:59:22 +0100

    add more config options

--- aiko2/config/config.py
+++ aiko2/config/config.py
@@ -26,3 +26,17 @@ class Config:
 
     # The system instructions for the assistant.
     instructions: str = "You are an AI assistant. You are to assist users in their tasks. Be polite and helpful."
+
+    # Maximum number of input characters.
+    max_input_length: int = 4096
+
+    # Length of window used to cut off tokens when len(tokens) > max_input_length
+    cut_off_window: int = 2048
+
+    # whether to log the conversations
+    loggings: bool = False
+
+    # where to log the conversations
+    log_dir: str = "logs"
+
+    
