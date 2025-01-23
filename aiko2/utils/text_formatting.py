601487b61aaff8847a51e63bec26ec79940e204e
Ben <benschumacher0@gmail.com>
2025-01-23 18:26:37 +0100

    add util for splitting text

---
+++ aiko2/utils/text_formatting.py
@@ -0,0 +1,38 @@
+def split_text(self, text:str, max_length=1000) -> list[str]:
+        """
+        Split a long text into smaller chunks.
+        If possible, try to split the text at logical points (e.g. at the end of a sentence).
+        
+        Parameters
+        ----------
+        text : str
+            The text to split.
+        
+        Returns
+        -------
+        list[str]
+            A list of smaller chunks of text.
+        """
+        seperators = ['\n\n', '\n', '. ', '! ', '? ']
+
+        good_chunks = []
+
+        while len(text) > max_length:
+            chunk = text[:max_length]
+            seperator = None
+            for sep in seperators:
+                if sep in chunk:
+                    seperator = sep
+                    break
+            if seperator:
+                last_pos = chunk.rfind(seperator)
+                good_chunks.append(chunk[:last_pos])
+                text = text[last_pos + len(seperator):]
+
+            else:
+                # no good seperator found, just split at max_length
+                good_chunks.append(chunk)
+                text = text[max_length:]
+            
+        good_chunks.append(text)
+        return good_chunks
