b5572f5a296d2cf0a1fa57b58d8c342cea3cb2ac
ben <benschumacher0@gmail.com>
2025-01-24 15:47:38 +0100

    add embedder to utils

--- aiko2/utils/__init__.py
+++ aiko2/utils/__init__.py
@@ -1,3 +1,4 @@
 from .estimate_tokens import estimate_tokens
 from .storage_location import get_storage_location
 from .text_formatting import split_text
+from .text_embedding import embed_text, get_embedder
