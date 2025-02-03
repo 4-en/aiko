659195456bdbd1bf2f14ca602c4bc97d4c71ff55
ben <benschumacher0@gmail.com>
2025-02-03 11:33:57 +0100

    add context refiner class

--- aiko2/retriever/__init__.py
+++ aiko2/retriever/__init__.py
@@ -4,3 +4,4 @@ from .web_retriever import WebRetriever
 from .ranking import BaseRanker, RankerResult, register_ranker
 from .memory_retriever import MemoryRetriever
 from .retrieval_router import RetrievalRouter, RoutingScoreFunction, domain_routing_function, query_type_routing_function, negated_routing_function
+from .context_refiner import ContextRefiner
