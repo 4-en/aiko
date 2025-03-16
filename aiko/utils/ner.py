02ec13e229964abd4eb133ee593ed8b557186755
Ben <benschumacher0@gmail.com>
2025-03-16 22:09:30 +0100

    only return entities

--- aiko/utils/ner.py
+++ aiko/utils/ner.py
@@ -6,7 +6,7 @@ class NER:
     nlp = spacy.load("en_core_web_sm")
 
     @staticmethod
-    def get_entities(text: str):
+    def get_entities(text: str) -> list[str]:
         """
         Get named entities from a text.
         
@@ -18,8 +18,8 @@ class NER:
         Returns
         -------
         list
-            A list of tuples with the entity text and the entity label
+            A list of entities.
         """
         doc = NER.nlp(text)
         allowed_entities = {"PERSON", "ORG", "GPE", "NORP", "FAC", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART", "LAW", "LANGUAGE"}
-        return [(ent.text, ent.label_) for ent in doc.ents if ent.label_ in allowed_entities]
+        return [ent.text for ent in doc.ents if ent.label_ in allowed_entities]
