474bd2accd29843be3dc73d3a90b6856db2c66ef
Ben <benschumacher0@gmail.com>
2025-03-16 22:11:19 +0100

    add getter for nlp

--- aiko/utils/ner.py
+++ aiko/utils/ner.py
@@ -3,7 +3,19 @@
 import spacy
 
 class NER:
-    nlp = spacy.load("en_core_web_sm")
+    _nlp = spacy.load("en_core_web_sm")
+
+    @staticmethod
+    def get_nlp() -> spacy.language.Language:
+        """
+        Get the NLP model.
+
+        Returns
+        -------
+        spacy.language.Language
+            The NLP model.
+        """
+        return NER._nlp
 
     @staticmethod
     def get_entities(text: str) -> list[str]:
@@ -20,6 +32,6 @@ class NER:
         list
             A list of entities.
         """
-        doc = NER.nlp(text)
+        doc = NER._nlp(text)
         allowed_entities = {"PERSON", "ORG", "GPE", "NORP", "FAC", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART", "LAW", "LANGUAGE"}
         return [ent.text for ent in doc.ents if ent.label_ in allowed_entities]
